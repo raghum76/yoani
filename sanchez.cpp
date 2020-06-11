a00514e139b7bb75b41bbdc70573d3fd23557138
raghum76 <raghum76@ucol.mx>
2020-06-11 14:20:02 -0500

    update1

--- sanchez.cpp
+++ sanchez.cpp
@@ -12,17 +12,34 @@ int main()
 	do{
 		op=menu();
 		switch(op){
-			case 1:
+			case 1:  // guardar datos
+				cout<<"Guardando datos en vector";
+				ofstream archSalida ("sanchez.dat",ios::binary||ios::out);
+				int nc;
+				int cal;
+				char nombre[20];
+				int otro='s';
+				while (otro=='s' || otro=='S'){
+					cout<<"Dame numero de cuenta nombre y calificacion";
+					cin>>nc;
+					cin.getline(nombre,20,'\n');
+					cin>>cal;
+					archSalida.write((char *)&nc,sizeof(int));
+					archSalida.write(nombre,20);
+					archSalida.write((char *)&cal,sizeof(int));
+					cout<<"Otro dato (s/n)";
+					cin>>otro;
+				}
+
+				archSalida.close();
 				break;
-			case 2:
+			case 2:  //mostrar datos
 				break;
-			case 3:
+			case 3:  //guardar vector
 				break;
-			case 4:
+			case 4:  //mostrar vector
 				break;
-			case 5:
-				break;
-			case 6:
+			case 5:  //primero y ultimo
 				break;
 		}
 
